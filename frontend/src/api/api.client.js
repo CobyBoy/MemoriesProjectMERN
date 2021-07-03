d3ea0c0b55860b68c3c9734b25deba5ce24c256a
CobyBoy <aboutacoby@gmail.com>
2021-07-03 02:59:46 -0300

    Added api constant

--- frontend/src/api/api.client.js
+++ frontend/src/api/api.client.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
+import { API_URL } from '../constants/api'
 
-const url = 'https://cobymemories.herokuapp.com/posts';
-
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const fetchPosts = () => axios.get(API_URL);
+export const createPost = (newPost) => axios.post(API_URL, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${API_URL}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${API_URL}/${id}`);
+export const likePost = (id) => axios.patch(`${API_URL}/${id}/likePost`);
