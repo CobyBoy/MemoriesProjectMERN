0bdcd94fb7ba15adbaeb37d1cc0a1042c612eba8
CobyBoy <aboutacoby@gmail.com>
2022-02-11 18:30:49 -0300

    Fixed problem with Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk';
 import reducers from './redux/index'
 import App from './App'
 
-const store = createStore(reducers, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(reducers, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (a) => a))
 ReactDOM.render(
     <Provider store = {store}>
         <App />
